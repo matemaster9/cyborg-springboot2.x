1a629d7e3e0e8aaf743378c09eb1ea2e5be0abd2
cyborg <matemaster@163.com>
2023-05-18 10:32:44 +0800

    init

---
+++ src/main/java/org/mastercs/cyborg/Springboot2xApplication.java
@@ -0,0 +1,15 @@
+package org.mastercs.cyborg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: cyborg
+ * Create: 2023/5/18 10:26
+ */
+@SpringBootApplication
+public class Springboot2xApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2xApplication.class, args);
+    }
+}
---
+++ src/main/java/org/mastercs/cyborg/common/HardwareInfoSupplier.java
@@ -0,0 +1,8 @@
+package org.mastercs.cyborg.common;
+
+/**
+ * Author: cyborg
+ * Create: 2023/5/18 10:31
+ */
+public class HardwareInfoSupplier {
+}
