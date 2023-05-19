8f7c61f2f7e9eb827fc5323a6422359edaddcbe8
syler <matemaster@163.com>
2023-05-19 14:15:54 +0800

    springdoc init

--- src/main/java/org/mastercs/cyborg/controller/SystemInfoController.java
+++ src/main/java/org/mastercs/cyborg/controller/SystemInfoController.java
@@ -1,5 +1,7 @@
 package org.mastercs.cyborg.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.mastercs.cyborg.facade.SystemInfoFacade;
 import org.mastercs.cyborg.model.DashboardVO;
@@ -10,11 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/system-info")
 @RequiredArgsConstructor
+@Tag(name = "SystemInfoController")
 public class SystemInfoController {
 
     private final SystemInfoFacade systemInfoFacade;
 
     @GetMapping("/getDashboard")
+    @Operation(summary = "getDashboard")
     public DashboardVO getDashboard() {
         return systemInfoFacade.getDashboard();
     }
---
+++ src/main/java/org/mastercs/cyborg/swagger/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package org.mastercs.cyborg.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("OpenAPI")
+                        .version("1.0")
+                        .description("Open API for demonstrating Swagger in Spring Boot"));
+    }
+}
